6a9ad2cc00c15258f9f1b263d8b1c88f6fee45a9
AndrewTakayama <Takayamaa19@avphs.net>
2019-09-07 14:28:55 -0700

    I converted inches to feet and had it displayed on the monitor

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,23 @@
 // This program calculates a person's height in feet (') and inches (").
+#include <iostream>
+int main ()
+{
+  int inches;
+  int feet;
+  int moduleinch;
+// Asks for person in inches
+std::cout << "How tall is this person in inches?\n";
+std::cin  >> inches;
+
+// Converts the inches to feet.
+feet = inches / 12;
+
+//Convers feet to inches.
+moduleinch = inches % 12;
+
+//Displays the feet of the person.
+std::cout << "This person is" <<feet<< "\'" <<moduleinch<< "\"";
+
+return 0;
+
+}
